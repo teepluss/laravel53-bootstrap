55414b1307c2897d303cb16dbfa4d1988360f152
Teepluss <admin@jquerytips.com>
2016-08-30 10:16:19 +0700

    core of concept

--- app/Traits/AccessControl.php
+++ app/Traits/AccessControl.php
@@ -4,23 +4,48 @@ namespace App\Traits;
 
 trait AccessControl
 {
+    /**
+     * Preserve for "superadmin" slug.
+     *
+     * @var string
+     */
     private $superadmin = "superadmin";
 
+    /**
+     * Get user roles.
+     *
+     * @return collection
+     */
     public function getRoles()
     {
         return $this->roles;
     }
 
+    /**
+     * Get user role key by slug.
+     *
+     * @return collection
+     */
     public function getKeyRoles()
     {
         return $this->getRoles()->keyBy('slug');
     }
 
+    /**
+     * Get user permissions.
+     *
+     * @return collection
+     */
     public function getPermissions()
     {
         return $this->permissions;
     }
 
+    /**
+     * Get role permissions.
+     *
+     * @return collection
+     */
     public function getRolePermissions()
     {
         $combinePermissions = collect([]);
@@ -32,6 +57,11 @@ trait AccessControl
         return $combinePermissions;
     }
 
+    /**
+     * Get merge permissions for user and roles.
+     *
+     * @return collection
+     */
     public function getMergedPermissions()
     {
         $personalPermissions = $this->getPermissions();
@@ -40,11 +70,23 @@ trait AccessControl
         return $rolePermissions->merge($personalPermissions);
     }
 
+    /**
+     * Check the user role.
+     *
+     * @param  string  $role
+     * @return boolean
+     */
     public function hasRole($role)
     {
         return $this->getKeyRoles()->has($role);
     }
 
+    /**
+     * Check the user role case OR.
+     *
+     * @param  array   $roles
+     * @return boolean
+     */
     public function hasAnyRole(array $roles)
     {
         if (! empty($roles)) foreach ($roles as $role) {
@@ -55,6 +97,12 @@ trait AccessControl
         return false;
     }
 
+    /**
+     * Check the user role case AND.
+     *
+     * @param  array   $roles
+     * @return boolean
+     */
     public function hasAllRole(array $roles)
     {
         if (! empty($roles)) foreach ($roles as $role) {
@@ -65,11 +113,23 @@ trait AccessControl
         return true;
     }
 
+    /**
+     * Check the user permit.
+     *
+     * @param  string  $requestPermit
+     * @return boolean
+     */
     public function hasPermission($requestPermit)
     {
         return (boolean) $this->getMergedPermissions()->get($requestPermit);
     }
 
+    /**
+     * Check the user permit case OR.
+     *
+     * @param  array   $permissions
+     * @return boolean
+     */
     public function hasAnyPermission(array $permissions)
     {
         if (! empty($permissions)) foreach ($permissions as $permit) {
@@ -80,6 +140,12 @@ trait AccessControl
         return false;
     }
 
+    /**
+     * Check the user permit case AND.
+     *
+     * @param  array   $permissions
+     * @return boolean
+     */
     public function hasAllPermission(array $permissions)
     {
         if (! empty($permissions)) foreach ($permissions as $permit) {
@@ -90,6 +156,11 @@ trait AccessControl
         return true;
     }
 
+    /**
+     * Check superadmin permit.
+     *
+     * @return boolean
+     */
     public function isSuperadmin()
     {
         return (boolean) $this->hasPermission($this->superadmin);
